a3a41c38256875150a52ff555d7186b71dcd6765
pawel <pawel.zboinski@neoteric.eu>
2019-02-20 18:05:24 +0100

    36 m

--- reference/fs_demo.js
+++ reference/fs_demo.js
@@ -9,9 +9,27 @@ const path = require('path');
 // });
 
 //create and write to file
-fs.writeFile (path.join(__dirname, '/test', 'hello.txt'),
-  'Hello World ;)',
-  err => {
-    if (err) throw err;
-    console.log('Folder written to...');
-  });
+// fs.writeFile (path.join(__dirname, '/test', 'hello.txt'),
+//   'Hello World ;)',
+//   err => {
+//     if (err) throw err;
+//     console.log('Folder written to...');
+//
+//     fs.appendFile (path.join(__dirname, '/test', 'hello.txt'),
+//       '    Daje rade',
+//       err => {
+//         if (err) throw err;
+//         console.log('Folder written to...');
+//       });
+//   });
+
+// read file
+fs.readFile(path.join(__dirname, '/test', 'hello.txt'), 'utf8',  (err,  data) => {
+  if (err) throw err;
+  console.log(data);
+});
+
+
+// rename file
+
+fs.rename(path.join())
