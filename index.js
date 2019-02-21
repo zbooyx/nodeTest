84a0d8dc13024d136c388907dd6bd5567920188b
pawel <pawel.zboinski@neoteric.eu>
2019-02-21 16:21:31 +0100

    1h 03m

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
-const Logger = require('./logger');
+// const Logger = require('./logger');
+//
+// const logger = new Logger();
+// logger.on('message', (data) => console.log('Called Listener', data));
+//
+// logger.log('hi wodddddrld');
+// logger.log('hiffffffff world');
+// logger.log('hi wogggggggggrld');
+// logger.log('hi wovvvvvvvvvvvvrld');
 
-const logger = new Logger();
-logger.on('message', (data) => console.log('Called Listener', data));
 
-logger.log('hi wodddddrld');
-logger.log('hiffffffff world');
-logger.log('hi wogggggggggrld');
-logger.log('hi wovvvvvvvvvvvvrld');
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+
+
+const server  = http.createServer((req, res) =>{
+  console.log(req.url);
+  if(req.url === '/'){
+    res.end('<h1> home </h1>')
+  }
+});
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => console.log(`Server runnung on port: ${PORT}`));
